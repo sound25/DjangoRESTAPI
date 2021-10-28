4f30928347ba430dd44c8b9b2f43eccd804d42cd
Soundarya <34751165+sound25@users.noreply.github.com>
2021-10-28 23:37:02 +0530

    added 2 viewsets, diff end points
    added 2 viewsets

--- API/API/urls.py
+++ API/API/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from rest_framework import routers
+from movies.views import MovieViewSet,ActionViewSet,DramaViewSet
+
+router=routers.SimpleRouter()
+router.register('movies',MovieViewSet)
+router.register('action',ActionViewSet)
+router.register('drama',DramaViewSet)
 
 urlpatterns = [
+    path('',include(router.urls)),
     path('admin/', admin.site.urls),
 ]
