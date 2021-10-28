0f898eff5e366eaea4233361c8c99660b2536a40
Soundarya <34751165+sound25@users.noreply.github.com>
2021-10-28 23:47:09 +0530

    configured image field
    configured image field

--- API/movies/models.py
+++ API/movies/models.py
@@ -6,6 +6,6 @@ class Moviedata(models.Model):
     duration=models.FloatField()
     rating=models.FloatField()
     typ=models.CharField(max_length=200,default='action')
-
+    image=models.ImageField(upload_to='images/',default='images/None/NoImg.png')
     def __str__(self):
         return self.name
