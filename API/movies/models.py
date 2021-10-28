26d97517bb390d103c6bfc7415138646a2c7a5f0
Soundarya <34751165+sound25@users.noreply.github.com>
2021-10-28 20:59:24 +0530

    Movies Model Added
    Movies Model Added

--- API/movies/models.py
+++ API/movies/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Moviedata(models.Model):
+    name=models.CharField(max_length=200)
+    duration=models.FloatField()
+    rating=models.FloatField()
